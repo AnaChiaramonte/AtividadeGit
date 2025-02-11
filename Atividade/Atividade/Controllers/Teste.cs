641873ac3c9ed0783757756a29e185e95025f169
Ana Chiaramonte <anasenai.lopes@gmail.com>
2025-02-11 16:10:39 -0300

    Criando a Classe teste

---
+++ Atividade/Atividade/Controllers/Teste.cs
@@ -0,0 +1,9 @@
+﻿namespace Atividade.Controllers
+{
+    public class Teste
+    {
+        public string LivroId { get; set; }
+        public string Categoria { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
