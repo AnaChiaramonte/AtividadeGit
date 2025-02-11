66a2d513c43ac08dca8c3a7f5ff06dce9e45c4a1
PEDRO ARAUJO <mxpedro.araujo11@gmail.com>
2025-02-11 16:09:46 -0300

    Testando git hub e Branches

---
+++ Atividade/Atividade/Controllers/bibliotecaTeste.cs
@@ -0,0 +1,14 @@
+﻿namespace Atividade.Controllers
+{
+    public class bibliotecaTeste
+    {
+        public string teste()
+        {
+            return "Teste";
+        }
+        public int soma(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
